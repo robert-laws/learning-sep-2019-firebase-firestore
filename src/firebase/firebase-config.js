93cd3f12310912f8cb8f30e16206a7cb580f96b1
Robert Laws <roblaws@me.com>
2019-09-22 13:12:50 +0300

    basic firebase configuration

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAX2yomnyo67YqFlh-sBdUeElMVTuPTFyY",
+  authDomain: "sep-2019-firebase-firestore.firebaseapp.com",
+  databaseURL: "https://sep-2019-firebase-firestore.firebaseio.com",
+  projectId: "sep-2019-firebase-firestore",
+  storageBucket: "sep-2019-firebase-firestore.appspot.com",
+  messagingSenderId: "761834369558",
+  appId: "1:761834369558:web:c3b759253fc684c8603b37"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+export default firebase;
